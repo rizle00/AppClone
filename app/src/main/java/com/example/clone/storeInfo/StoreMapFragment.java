0a68962c33884d0a8c2ea535c02b8e504fe1bc56
rizle00 <144658576+rizle00@users.noreply.github.com>
2023-12-19 17:52:01 +0900

    12.19

--- app/src/main/java/com/example/clone/storeInfo/StoreMapFragment.java
+++ app/src/main/java/com/example/clone/storeInfo/StoreMapFragment.java
@@ -11,12 +11,12 @@ import android.view.ViewGroup;
 import com.example.clone.R;
 
 
-public class StoreInfoFragment extends Fragment {
+public class StoreMapFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store_info, container, false);
+        return inflater.inflate(R.layout.fragment_store_map, container, false);
     }
 }
