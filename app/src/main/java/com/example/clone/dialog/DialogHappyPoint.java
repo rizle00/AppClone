9f806a0499758e1cc1b38e4e075523cbc58acdce
hwangse84 <144659679+hwangse84@users.noreply.github.com>
2023-12-19 14:24:42 +0900

    !

--- app/src/main/java/com/example/clone/dialog/DialogHappyPoint.java
+++ app/src/main/java/com/example/clone/dialog/DialogHappyPoint.java
@@ -1,10 +1,6 @@
 package com.example.clone.dialog;
 
 import android.app.Dialog;
-import android.content.Context;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 
 import com.example.clone.databinding.DialogHappyPointBinding;
 
@@ -12,7 +8,7 @@ public class DialogHappyPoint extends Dialog {
 
     DialogHappyPointBinding binding;
 
-    public DialogHappyPoint(@NonNull Context context) {
+    public DialogHappyPoint() {
         super(context);
         binding=DialogHappyPointBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
