5d3adae64fe206896dba447bad5d8128240e0a85
rizle00 <144658576+rizle00@users.noreply.github.com>
2023-12-20 11:57:34 +0900

    12.20

---
+++ app/src/main/java/com/example/clone/order/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.clone.order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.clone.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
